8a69b623b3637b79cbe4d626235cf650d915493f
Amardeep095 <1324Amardeep@gmail.com>
2026-05-06 12:08:31 +0530

    ok

--- 1.ChatModels/2_chatmodel_google.py
+++ 1.ChatModels/2_chatmodel_google.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-result = model.invoke('what is the capital of india? ?')
+result = model.invoke('what is the capital of india?')
 
 print(result.content)
 
